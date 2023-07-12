844a79dac313176bf293d594aaf4a241831cabaa
HectorOrtegaRo <hector.max.za@gmail.com>
2023-07-12 12:01:32 -0600

    Primer commit

--- hector/src/app/login/page.js
+++ hector/src/app/login/page.js
@@ -1,12 +1,38 @@
+`use client`
+import { useState } from "react";
 import "./login.css"
 
 export default function login(); {
 
+    const [state, setState] = useState({
+        email: "",
+        password: ""
+    });
+
+    const handleChange = (event) => {
+        console.log(event)
+        const { name, value } = event.target;
+        setState({
+            ...state, 
+            [name]: value
+        })
+    }
+
     return (
         <div className="container">
         
-            <input type="text" placeholder="email"/>
-            <input type="password" placeholder="123456"/>
+            <input onChance={handleChange} 
+            name="email" 
+            value={state.email} 
+            type="text" 
+            placeholder="email"/>
+            <input 
+            name="password" 
+            onChange={handleChange} 
+            type="password" 
+            placeholder="123456"/>
+
+            
             <button>Login</button>
 
         </div>
